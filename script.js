3fa47294984042b62b516d6f8b56ff5e11d8dcf9
nikhil pandey <105156966+nikhil-pandey-digital@users.noreply.github.com>
2026-03-25 15:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 window.addEventListener('DOMContentLoaded',()=>{
 	let Ele= document.createElement('h1');
-	Ele.innerText='DOM load Success'
+	Ele.innerText='DOM load success'
 	document.body.appendChild(Ele);
 	
 })
